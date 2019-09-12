1fa472c4199940473baa169e3c52af21c1c463f6
Ganesh Kumar Das <ganesh123online@gmail.com>
2019-09-12 12:14:11 +0530

    Hello there

---
+++ app/src/main/java/com/packtpub/inflatelayout/MainActivity.java
@@ -0,0 +1,21 @@
+package com.packtpub.inflatelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickLeft(View view) {
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickRight(View view) {
+        setContentView(R.layout.activity_main);
+    }
+}
